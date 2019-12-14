0ca30f1febaf66218bb93067630e40af9971d428
Dirus <solankie97.deepesh@gmail.com>
2019-12-14 12:18:50 +0530

    Final commit

--- hello.go
+++ hello.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
-const hello = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
 
 // Hello ... utils func
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return hello + name
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spanishPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
+}
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
